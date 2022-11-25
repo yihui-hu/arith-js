b539563e7ed588e77e66172959f7f5f7e89ab232
yihui <90987235+yihui-hu@users.noreply.github.com>
2022-11-24 20:02:18 -0500

    first build of arith, complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import "../styles/Header.css"
 import arith_logo from "../assets/arith_logo.svg"
+<<<<<<< HEAD
 import info_icon from "../assets/info_icon.svg"
 import useMediaQuery from "../hooks/useMediaQuery"
 import { motion } from "framer-motion"
@@ -24,6 +25,27 @@ const Header = () => {
       <hr className={isDesktop ? "hr" : "hr_mobile"}></hr>
     </motion.div>
   )
+=======
+import arith_wordmark from "../assets/arith_wordmark.svg"
+import info_icon from "../assets/info_icon.svg"
+import useMediaQuery from "../hooks/useMediaQuery"
+
+const Header = () => {
+    const isDesktop = useMediaQuery('(min-width: 768px)');
+
+    return (
+        <div className="header">
+            <div className="header_info">
+                <div className="header_left">
+                    <img src={arith_wordmark} className={isDesktop ? "arith_logo" : "arith_logo_mobile"}/>
+                    {/* <h3 id="header_text">is a ppm compressor/decompressor.</h3> */}
+                </div>
+                    <a href="#info"><img src={info_icon} className={isDesktop ? "info_icon" : "info_icon_mobile"}/></a>
+            </div>
+            <hr className={isDesktop ? "hr" : "hr_mobile"}></hr>
+        </div>
+    )
+>>>>>>> 86aa03f (first build of arith, complete)
 }
 
 export default Header
