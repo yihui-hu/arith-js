4de9f22111d2682467a50603f4d792cdb4ab7bc4
yihui <90987235+yihui-hu@users.noreply.github.com>
2022-11-24 21:20:27 -0500

    more edits to notes/info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -22,7 +22,7 @@ const Info = () => {
                     <br></br>
                     <h4 className="header_text">How does it work?</h4>
                     <hr></hr>
-                    <h4>The compression algorithm relies on bitpacking, quantization, as well as discrete cosine transformations. Briefly speaking, a ppm (P6) image is read in and processed, and information not easily discernable by the naked eye is discard. Nearing the end of compression, what info remains about a single pixel is packed into 4 bytes instead of, say, 8-12 bytes. So that's quite a lot of space saved!
+                    <h4>The compression algorithm relies on bitpacking, quantization, as well as discrete cosine transformations. Briefly speaking, a ppm (P6) image is read in and processed, and information not easily discernable by the naked eye is discard. At the end, what info remains about a single pixel is packed into 1 byte instead of, say, 4-8 bytes. So that's quite a lot of space saved!
                     </h4>
                     <br></br>
                     <h4 className="header_text">Source files?</h4>
@@ -40,7 +40,7 @@ const Info = () => {
                     <br></br>
                     <h4 className="header_text">On porting C code to JavaScript</h4>
                     <hr></hr>
-                    <h4>JavaScript has no built in integer types, so manipulating bits is weird. I had to do it manually with strings: "101010". JavaScript also doesn't have native support for reading / writing all 256 ASCII characters, so I had to design my own decoding table that mapped each value between 0-255 to a UTF-8 character. There are other quirks with porting C to JavaScript; might do a more comprehensive write-up in a few.
+                    <h4>JS has no built in integer types, so manipulating bits is weird. I had to do it manually with strings: "101010". JS also doesn't have native support for reading / writing all 256 ASCII characters, so I had to design my own decoding table that mapped each value between 0-255 to a UTF-8 character. There are other quirks with porting C to JS; might do a more comprehensive write-up in a few.
                     </h4>
                     <br></br>
                     <h4 className="header_text">Decompressor produces a P3 ppm image? <br></br> Why not P6 like the original?</h4>
