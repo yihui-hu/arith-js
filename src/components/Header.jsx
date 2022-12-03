4c7244551c1c73f702cc21c03f7577005a08c4bb
yihui <90987235+yihui-hu@users.noreply.github.com>
2022-12-02 20:24:35 -0500

    removed redundant code / comments, beginning proper documentation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import "../styles/Header.css"
 import arith_logo from "../assets/arith_logo.svg"
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ce605e8 (removed redundant code / comments, beginning proper documentation)
 import info_icon from "../assets/info_icon.svg"
 import useMediaQuery from "../hooks/useMediaQuery"
 import { motion } from "framer-motion"
 
 const Header = () => {
   const isDesktop = useMediaQuery('(min-width: 768px)');
+<<<<<<< HEAD
 
   return (
     <motion.div
@@ -46,6 +50,26 @@ const Header = () => {
         </div>
     )
 >>>>>>> 86aa03f (first build of arith, complete)
+=======
+
+  return (
+    <motion.div
+      className="header"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 1,
+        delay: 0.1,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}>
+      <div className="header_info">
+        <img src={arith_logo} className={isDesktop ? "arith_logo" : "arith_logo_mobile"} />
+        <a href="#info"><img src={info_icon} className={isDesktop ? "info_icon" : "info_icon_mobile"} /></a>
+      </div>
+      <hr className={isDesktop ? "hr" : "hr_mobile"}></hr>
+    </motion.div>
+  )
+>>>>>>> ce605e8 (removed redundant code / comments, beginning proper documentation)
 }
 
 export default Header
