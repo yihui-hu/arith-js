4765867e5f2a0a4e6ac89f3dce677d8a479bed68
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-05-28 11:32:18 -0400

    Fixed merge conflict markers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,11 @@
 import "../styles/Header.css"
 import arith_logo from "../assets/arith_logo.svg"
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ce605e8 (removed redundant code / comments, beginning proper documentation)
 import info_icon from "../assets/info_icon.svg"
 import useMediaQuery from "../hooks/useMediaQuery"
 import { motion } from "framer-motion"
 
 const Header = () => {
   const isDesktop = useMediaQuery('(min-width: 768px)');
-<<<<<<< HEAD
 
   return (
     <motion.div
@@ -29,47 +24,6 @@ const Header = () => {
       <hr className={isDesktop ? "hr" : "hr_mobile"}></hr>
     </motion.div>
   )
-=======
-import arith_wordmark from "../assets/arith_wordmark.svg"
-import info_icon from "../assets/info_icon.svg"
-import useMediaQuery from "../hooks/useMediaQuery"
-
-const Header = () => {
-    const isDesktop = useMediaQuery('(min-width: 768px)');
-
-    return (
-        <div className="header">
-            <div className="header_info">
-                <div className="header_left">
-                    <img src={arith_wordmark} className={isDesktop ? "arith_logo" : "arith_logo_mobile"}/>
-                    {/* <h3 id="header_text">is a ppm compressor/decompressor.</h3> */}
-                </div>
-                    <a href="#info"><img src={info_icon} className={isDesktop ? "info_icon" : "info_icon_mobile"}/></a>
-            </div>
-            <hr className={isDesktop ? "hr" : "hr_mobile"}></hr>
-        </div>
-    )
->>>>>>> 86aa03f (first build of arith, complete)
-=======
-
-  return (
-    <motion.div
-      className="header"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{
-        duration: 1,
-        delay: 0.1,
-        ease: [0, 0.71, 0.2, 1.01]
-      }}>
-      <div className="header_info">
-        <img src={arith_logo} className={isDesktop ? "arith_logo" : "arith_logo_mobile"} />
-        <a href="#info"><img src={info_icon} className={isDesktop ? "info_icon" : "info_icon_mobile"} /></a>
-      </div>
-      <hr className={isDesktop ? "hr" : "hr_mobile"}></hr>
-    </motion.div>
-  )
->>>>>>> ce605e8 (removed redundant code / comments, beginning proper documentation)
 }
 
-export default Header
+export default Header;
